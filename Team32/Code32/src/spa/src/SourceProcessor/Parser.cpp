44cd274f1906bc895fd4a9fb6673bb7a0acb01ee
Lin Geyu <russellgy1229@gmail.com>
2021-09-01 10:15:44 +0800

    sketch the logic for parser

--- Team32/Code32/src/spa/src/SourceProcessor/Parser.cpp
+++ Team32/Code32/src/spa/src/SourceProcessor/Parser.cpp
@@ -2,16 +2,88 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-using namespace std;
-
+#include <unordered_map>
+#include <stack>
+#include "../Tokenizer/Token/SimpleToken.h"
+#include "../Tokenizer/SimpleTokenizer.h"
 #include "PKB.h"
 #include "TNode.h"
 #include "Parser.h"
+#include "../Utils/StmtType.h"
+#include <bits/stdc++.h>
+
+using namespace simple;
 
 class ParserImp: Parser {
+private:
+    unordered_map<int, simple::StmtType> stmtsTypeMap;
+    unordered_map<int, vector<simple::SimpleToken>> stmtsTokenMap;
+
+    void constructStmtsTokenMap(const vector<simple::SimpleToken>& tokens) {
+
+    }
+
+    void unordered_map<int, simple::StmtType>> constructStmtsTypeMap(const vector<simple::SimpleToken> tokens) {
+
+    }
+
+    vector<int> getStmtsListForContainer(int containerStmtNum) {
+
+    }
+
+    //Recursive resolve a program
+    void resolveProgram(int stmtsNumber, vector<int> stmtsList) {
+        int size = stmtsList.size();
+
+        for (int i = 0; i < size - 1; i++) {
+            stmtNum = stmtNums[i];
+            simple::StmtType currType = stmtsTypeMap[stmtNum];
+            simple::StmtType nextType = stmtsTypeMap[stmtNum + 1];
+
+            if (isContainer(currType)) {
+                // When the current statement is a container statement
+                vector<int> newStmtsList = getStmtsListForContainer(stmtNum);
+                // TODO: add stmts List as parent relationship
+                resolveProgram(stmtNum + 1, newStmtsList);
+                i += newStmtsList.size();
+                // Update i with regards to the statement list for
+            } else {
+                //TODO: Add parent relationship
+            }
+        }
+    }
+
+    vector<int> getStmtsNums(unordered_map<int, simple::StmtType> map) {
+        vector<int> keySet;
+
+        for (auto const& map: entry) {
+            keySet.push_back(map.first);
+        }
+
+        int n = sizeof(keySet) / sizeof(keySet[0])
+        return sort(keySet, keySet + n);
+    }
+
+    bool isContainer(StmtType type) {
+        return type == if_stmt || type == while_stmt;
+    }
 public:
+
     int parse(string& inputs) final {
 
+        //Tokenize the raw inputs
+        vector<simple::SimpleToken> tokens = simple::SimpleTokenizer::tokenize(inputs);
+        // Construct the required mapping
+        constructStmtsTokenMap(tokens);
+        constructStmtsTypeMap(tokens);
+
+        // Build relationship using the map
+
+        // Get the statements set
+        vector<int> stmtNums = getStmtsNums(stmtsTypeMap);
+
+        resolveProgram(stmtNums[0], stmtNums);
+
+        return 0;
     }
 };
---
+++ Team32/Code32/src/spa/src/Utils/StmtType.h
@@ -0,0 +1,15 @@
+//
+// Created by geyul on 1/9/2021.
+//
+
+#ifndef AUTOTESTER_STMTTYPE_H
+#define AUTOTESTER_STMTTYPE_H
+
+namespace simple {
+    enum class StmtType {
+        assign,
+        if_stmt,
+        while_stmt,
+    };
+}
+#endif //AUTOTESTER_STMTTYPE_H
