afb4a544ed8373abe61c1f4574f97a88c509cd68
bchenghi <bohchenghin@gmail.com>
2021-09-12 21:08:04 +0800

    Update result projector

--- Team32/Code32/src/spa/src/pql/QueryResultProjector/QueryResultProjector.cpp
+++ Team32/Code32/src/spa/src/pql/QueryResultProjector/QueryResultProjector.cpp
@@ -1,7 +1,13 @@
 #include "QueryResultProjector.h"
+#include <iostream>
 
 using pql::QueryResultProjector;
+using std::cout;
 
 std::string QueryResultProjector::outputResult(std::vector<std::string> results) {
+    for (std::string result : results) {
+        cout << result;
+        cout << "\n";
+    }
     return "";
 }
