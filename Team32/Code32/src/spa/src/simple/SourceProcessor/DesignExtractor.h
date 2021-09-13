dc18730aee03b9df5ebfea6dd017b13df6497407
Lingy12 <russellgy1229@gmail.com>
2021-09-13 16:58:20 +0800

    reformat the definition of Design Extractor

--- Team32/Code32/src/spa/src/simple/SourceProcessor/DesignExtractor.h
+++ Team32/Code32/src/spa/src/simple/SourceProcessor/DesignExtractor.h
@@ -4,10 +4,35 @@
 
 #ifndef GUARD_DESIGNEXTRACTOR_H
 #define GURAD_DESIGNEXTRACTOR_H
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
 
-class DesignExtractor {
-public:
-    virtual void extractDesign();
-};
+// TODO: Change when PKB API finished
+// NOTE: Parent inverse should be one-to-one mapping
+typedef unordered_map<size_t, size_t> FollowTable, ParentInverseTable;
+typedef unordered_map<size_t, vector<size_t>> ParentTable;
+typedef unordered_map<size_t, string> UsesTable, ModifiesTable;
+typedef vector<vector<size_t>> Graph; // Use adjacent list to represent graph
+
+namespace simple {
+    class DesignExtractor {
+    public:
+        void extractDesign();
+    private:
+        Graph generateFollowGraph(FollowTable followTable);
+        Graph generateParentGraph(ParentTable parentTable);
+        Graph generateTransitiveClosureFor(Graph graph);
+        void setRelationWithGraph(Graph graph);
+        void setUsesModifiesForContainer(UsesTable usesTable, ModifiesTable modifiesTable);
+
+        ParentInverseTable generateParentInverse(ParentTable parentTable);
+    };
+}
+
 
 #endif
