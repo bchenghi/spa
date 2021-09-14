0a5ea4350039314df933389f445d2500468f11f9
bchenghi <bohchenghin@gmail.com>
2021-09-14 16:52:39 +0800

    Add tests for query processor

---
+++ Team32/Code32/src/spa/src/pql/QueryProcessorManager.h
@@ -0,0 +1,20 @@
+#ifndef GUARD_QUERY_PROCESSOR_MANAGER_H
+#define GUARD_QUERY_PROCESSOR_MANAGER_H
+
+#include <string>
+#include "pql/QueryEvaluator/QueryEvaluator.h"
+#include "pql/QueryPreprocessor/Preprocessor.h"
+
+
+namespace pql {
+    class QueryProcessorManager {
+        QueryEvaluator queryEvaluator;
+        Preprocessor queryPreprocessor;
+    public:
+        QueryProcessorManager();
+        set<string> executeQuery(std::string);
+    };
+}
+
+
+#endif //GUARD_QUERY_PROCESSOR_MANAGER_H
