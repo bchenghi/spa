ed92087f0c57d1be5fb437676140d649b3ebbe70
Nauw1010 <57137372+Nauw1010@users.noreply.github.com>
2021-09-14 16:47:59 +0800

    Fix bugs (#56)

--- Team32/Code32/src/spa/src/PKB/ConstantTable.cpp
+++ Team32/Code32/src/spa/src/PKB/ConstantTable.cpp
@@ -1,14 +1,14 @@
 #include "ConstantTable.h"
 
-LIST_OF_CONSTANT constantsSet;
+LIST_OF_CONSTANT ConstantTable::constantsSet;
 
 bool ConstantTable::addConstant(int c)
 {
-	if (constantsSet.count(c) == 1) {
+	if (ConstantTable::constantsSet.count(c) == 1) {
 		return true;
 	}
 	else {
-		constantsSet.insert(c);
+		ConstantTable::constantsSet.insert(c);
 		return true;
 	}
 	return false;
@@ -16,7 +16,7 @@ bool ConstantTable::addConstant(int c)
 
 bool ConstantTable::ifConstant(int c)
 {
-	if (constantsSet.count(c) == 1) {
+	if (ConstantTable::constantsSet.count(c) == 1) {
 		return true;
 	}
 	else {
@@ -27,5 +27,5 @@ bool ConstantTable::ifConstant(int c)
 
 LIST_OF_CONSTANT ConstantTable::getAllConstant()
 {
-	return constantsSet;
+	return ConstantTable::constantsSet;
 }
