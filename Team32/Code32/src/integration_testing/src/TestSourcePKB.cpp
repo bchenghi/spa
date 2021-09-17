9042db80e165605f4e151dc015cf0e51cb085b3b
Lin Geyu <54443474+Lingy12@users.noreply.github.com>
2021-09-17 13:53:18 +0800

    Test/finalize test cases source pkb (#76)
    * complete 1 test cases
    
    * integration test for Source Processor with PKB
    
    * remove test for parser only

--- Team32/Code32/src/integration_testing/src/TestSourcePKB.cpp
+++ Team32/Code32/src/integration_testing/src/TestSourcePKB.cpp
@@ -5,10 +5,15 @@
 #include <simple/SourceProcessor/Parser.h>
 #include <simple/SourceProcessor/DesignExtractor.h>
 #include <PKB/UseTable.h>
+#include <PKB/ModifyTable.h>
 #include <Utils/TestUtils.h>
 
+typedef unordered_map<STMT_NO, STMT_NO> FollowType;
+typedef unordered_map<STMT_NO, LIST_OF_STMT_NO> FollowStarType, ParentStarType, ParentType;
+typedef unordered_map<STMT_NO, LIST_OF_VAR_NAME> UseType, ModifyType;
+
 TEST_CASE("While loop inside else block") {
-    SECTION("Parsing the statement") {
+    SECTION("Parsing the statement and extract design") {
         clearPKB();
 
         std::string source = "procedure test {\n"
@@ -31,8 +36,8 @@ TEST_CASE("While loop inside else block") {
     }
 
     SECTION("Check FollowTTable") {
-        unordered_map<STMT_NO, LIST_OF_STMT_NO> resFollowTTable = FollowTable::getFollowStarMap();
-        unordered_map<STMT_NO, LIST_OF_STMT_NO> expectedFollowTTable = {
+        FollowStarType resFollowTTable = FollowTable::getFollowStarMap();
+        FollowStarType expectedFollowTTable = {
                 {1, {2}},
                 {3, {4}},
                 {6, {7}},
@@ -42,12 +47,56 @@ TEST_CASE("While loop inside else block") {
     }
 
     SECTION("Check ParentTTable") {
-        unordered_map<STMT_NO, LIST_OF_STMT_NO> resParentTTable = ParentTable::getParentStarMap();
-        unordered_map<STMT_NO, LIST_OF_STMT_NO> expectedParentTTable = {
+        ParentStarType resParentTTable = ParentTable::getParentStarMap();
+        ParentStarType expectedParentTTable = {
                 {2, {3, 4, 5, 6, 7}},
                 {5, {6, 7}},
                 };
         REQUIRE(resParentTTable == expectedParentTTable);
+    }
+
+
+    SECTION("Check ParentTable") {
+        ParentType resParentTable = ParentTable::getParentMap();
+        ParentType expectedParentTable = {
+                {2, {3, 4, 5}},
+                {5, {6, 7}}
+        };
+        REQUIRE(resParentTable == expectedParentTable);
+    }
+
+    SECTION("Check FollowTable") {
+        FollowType resFollowTable = FollowTable::getFollowMap();
+        FollowType expectedFollowTable = {
+                {1, 2},
+                {3, 4},
+                {6, 7}
+        };
+        REQUIRE(resFollowTable == expectedFollowTable);
+    }
+
+    SECTION("Check Use Table") {
+        UseType resUseTable = UseTable::getStmtUseMap();
+        UseType expectedUseTable = {
+                {2, {"x"}},
+                {5, {"x"}},
+                {6, {"x"}}
+        };
+        REQUIRE(resUseTable == expectedUseTable);
+    }
+
+    SECTION("Check Modify Table") {
+        ModifyType resModifyTable = ModifyTable::getStmtModifyMap();
+        ModifyType expectedModifyTable = {
+                {1, {"x"}},
+                {2, {"x", "y", "z"}},
+                {3, {"y"}},
+                {4, {"z"}},
+                {5, {"x", "z"}},
+                {6, {"x"}},
+                {7, {"z"}}
+        };
+        REQUIRE(resModifyTable == expectedModifyTable);
     }
 }
 
@@ -81,78 +130,208 @@ TEST_CASE("Nested loop inside if block") {
 }
 
 TEST_CASE("While loop in if block") {
-    clearPKB();
-    std::string source = "procedure test {\n"
-                         "\tx = 1;\n"
-                         "\ty = 1;\n"
-                         "\n"
-                         "\tif (y == 1) then {\n"
-                         "\t\twhile (x < 10) {\n"
-                         "\t\t\tx = x + 1;\n"
-                         "\t\t\tk = 3;\n"
-                         "\t\t}\n"
-                         "\t} else {\n"
-                         "\t\tz = 3;\n"
-                         "\t\tz = z + 1;\n"
-                         "\t}\n"
-                         "}";
+    SECTION("Parse and Extract Design") {
+        clearPKB();
+        std::string source = "procedure test {\n"
+                             "\tx = 1;\n"
+                             "\ty = 1;\n"
+                             "\n"
+                             "\tif (y == 1) then {\n"
+                             "\t\twhile (x < 10) {\n"
+                             "\t\t\tx = x + 1;\n"
+                             "\t\t\tk = 3;\n"
+                             "\t\t}\n"
+                             "\t} else {\n"
+                             "\t\tz = 3;\n"
+                             "\t\tz = z + 1;\n"
+                             "\t}\n"
+                             "}";
 
-    Parser parser;
-    parser.parse(source);
+        Parser parser;
+        parser.parse(source);
 
-    DesignExtractor designExtractor;
-    designExtractor.extractDesign();
+        DesignExtractor designExtractor;
+        designExtractor.extractDesign();
+    }
 
+    SECTION("Check FollowTTable") {
+        FollowStarType resFollowTTable = FollowTable::getFollowStarMap();
+        FollowStarType expectedFollowTTable = {
+                {1, {2, 3}},
+                {2, {3}},
+                {5, {6}},
+                {7, {8}}
+                };
+
+        REQUIRE(resFollowTTable == expectedFollowTTable);
+    }
+
+    SECTION("Check ParentTTable") {
+        ParentStarType resParentTTable = ParentTable::getParentStarMap();
+        ParentStarType expectedParentTTable = {
+                {3, {4, 5, 6, 7, 8}},
+                {4, {5, 6}},
+                };
+        REQUIRE(resParentTTable == expectedParentTTable);
+    }
+
+
+    SECTION("Check ParentTable") {
+        ParentType resParentTable = ParentTable::getParentMap();
+        ParentType expectedParentTable = {
+                {3, {4, 7, 8}},
+                {4, {5, 6}}
+        };
+        REQUIRE(resParentTable == expectedParentTable);
+    }
+
+    SECTION("Check FollowTable") {
+        FollowType resFollowTable = FollowTable::getFollowMap();
+        FollowType expectedFollowTable = {
+                {1, 2},
+                {2, 3},
+                {5, 6},
+                {7, 8}
+        };
+        REQUIRE(resFollowTable == expectedFollowTable);
+    }
+
+    SECTION("Check Use Table") {
+        UseType resUseTable = UseTable::getStmtUseMap();
+        UseType expectedUseTable = {
+                {3, {"y", "x", "z"}},
+                {4, {"x"}},
+                {5, {"x"}},
+                {8, {"z"}}
+        };
+        REQUIRE(resUseTable == expectedUseTable);
+    }
+
+    SECTION("Check Modify Table") {
+        ModifyType resModifyTable = ModifyTable::getStmtModifyMap();
+        ModifyType expectedModifyTable = {
+                {1, {"x"}},
+                {2, {"y"}},
+                {3, {"x", "k", "z"}},
+                {4, {"x", "k"}},
+                {5, {"x"}},
+                {6, {"k"}},
+                {7, {"z"}},
+                {8, {"z"}}
+        };
+        REQUIRE(resModifyTable == expectedModifyTable);
+    }
 }
 
 TEST_CASE("Nested loop inside if block and statement after nested if statement") {
-    clearPKB();
-    std::string source = "procedure test {\n"
-                         "\tx = 1;\n"
-                         "\ty = 2;\n"
-                         "\n"
-                         "\tif (x > 0) then {\n"
-                         "\t\ty = y + 1;\n"
-                         "\n"
-                         "\t\tif (y > 2) then {\n"
-                         "\t\t\tz = 1;\n"
-                         "\t\t} else {\n"
-                         "\t\t\twhile (z < 100) {\n"
-                         "\t\t\t\tz = z + 1;\n"
-                         "\t\t\t}\n"
-                         "\t\t}\n"
-                         "z = 3;\n"
-                         "\t} else {\n"
-                         "\t\tk = 1;\n"
-                         "\t\tk = k + 1;\n"
-                         "\t}\n"
-                         "\tz = 10;\n"
-                         "}";
-    Parser parser;
-    parser.parse(source);
+    SECTION("Parse and Extract Design") {
+        clearPKB();
+        std::string source = "procedure test {\n"
+                             "\tx = 1;\n"
+                             "\ty = 2;\n"
+                             "\n"
+                             "\tif (x > 0) then {\n"
+                             "\t\ty = y + 1;\n"
+                             "\n"
+                             "\t\tif (y > 2) then {\n"
+                             "\t\t\tz = 1;\n"
+                             "\t\t} else {\n"
+                             "\t\t\twhile (z < 100) {\n"
+                             "\t\t\t\tz = z + 1;\n"
+                             "\t\t\t}\n"
+                             "\t\t}\n"
+                             "z = 3;\n"
+                             "\t} else {\n"
+                             "\t\tk = 1;\n"
+                             "\t\tk = k + 1;\n"
+                             "\t}\n"
+                             "\tz = 10;\n"
+                             "}";
+        Parser parser;
+        parser.parse(source);
 
-    DesignExtractor designExtractor;
-    designExtractor.extractDesign();
-}
+        DesignExtractor designExtractor;
+        designExtractor.extractDesign();
+    }
 
-TEST_CASE("While loop contains an if block") {
-    clearPKB();
-    std::string source = "procedure test {\n"
-                         "\tx = 1;\n"
-                         "\twhile (x < 10) {\n"
-                         "\t\tx = x + 1;\n"
-                         "\n"
-                         "\t\tif (x > 5) then {\n"
-                         "\t\t\ty = 1;\n"
-                         "\t\t} else {\n"
-                         "\t\t\tk = 1;\n"
-                         "\t\t}\n"
-                         "\t}\n"
-                         "}";
 
-    Parser parser;
-    parser.parse(source);
+    SECTION("Check FollowTTable") {
+        FollowStarType resFollowTTable = FollowTable::getFollowStarMap();
+        FollowStarType expectedFollowTTable = {
+                {1, {2, 3, 12}},
+                {2, {3, 12}},
+                {3, {12}},
+                {4, {5, 9}},
+                {5, {9}},
+                {10, {11}}
+        };
 
-    DesignExtractor designExtractor;
-    designExtractor.extractDesign();
+        REQUIRE(resFollowTTable == expectedFollowTTable);
+    }
+
+    SECTION("Check ParentTTable") {
+        ParentStarType resParentTTable = ParentTable::getParentStarMap();
+        ParentStarType expectedParentTTable = {
+                {3, {4, 5, 6, 7, 8, 9, 10, 11}},
+                {5, {6, 7, 8}},
+                {7, {8}}
+                };
+        REQUIRE(resParentTTable == expectedParentTTable);
+    }
+
+
+    SECTION("Check ParentTable") {
+        ParentType resParentTable = ParentTable::getParentMap();
+        ParentType expectedParentTable = {
+                {3, {4, 5, 9, 10, 11}},
+                {5, {6, 7}},
+                {7, {8}},
+        };
+        REQUIRE(resParentTable == expectedParentTable);
+    }
+
+    SECTION("Check FollowTable") {
+        FollowType resFollowTable = FollowTable::getFollowMap();
+        FollowType expectedFollowTable = {
+                {1, 2},
+                {2, 3},
+                {3, 12},
+                {4, 5},
+                {5, 9},
+                {10, 11}
+        };
+        REQUIRE(resFollowTable == expectedFollowTable);
+    }
+
+    SECTION("Check Use Table") {
+        UseType resUseTable = UseTable::getStmtUseMap();
+        UseType expectedUseTable = {
+                {3, {"y", "x", "z", "k"}},
+                {4, {"y"}},
+                {5, {"y", "z"}},
+                {7, {"z"}},
+                {8, {"z"}},
+                {11, {"k"}}
+        };
+        REQUIRE(resUseTable == expectedUseTable);
+    }
+
+    SECTION("Check Modify Table") {
+        ModifyType resModifyTable = ModifyTable::getStmtModifyMap();
+        ModifyType expectedModifyTable = {
+                {1, {"x"}},
+                {2, {"y"}},
+                {3, {"k", "z", "y"}},
+                {4, {"y"}},
+                {5, {"z"}},
+                {6, {"z"}},
+                {7, {"z"}},
+                {8, {"z"}},
+                {9, {"z"}},
+                {10, {"k"}},
+                {11, {"k"}},
+                {12, {"z"}}
+        };
+        REQUIRE(resModifyTable == expectedModifyTable);
+    }
 }
