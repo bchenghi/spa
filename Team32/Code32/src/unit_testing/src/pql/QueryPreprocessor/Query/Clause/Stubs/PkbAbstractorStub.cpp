83e96cb063a353eb18a2e25be5c7e48f6d952b4f
bchenghi <bohchenghin@gmail.com>
2021-09-28 14:12:20 +0800

    Update stubs

--- Team32/Code32/src/unit_testing/src/pql/QueryPreprocessor/Query/Clause/Stubs/PkbAbstractorStub.cpp
+++ Team32/Code32/src/unit_testing/src/pql/QueryPreprocessor/Query/Clause/Stubs/PkbAbstractorStub.cpp
@@ -37,7 +37,7 @@ namespace clausetest {
             return resultValueVarSet;
         }
 
-        virtual list<pair<StmtNum, VarName>> getPattern(StmtNum, const Value&, PostFixExpression) {
+        virtual list<pair<StmtNum, VarName>> getPattern(StmtNum, const Value&, PostFixExpression, bool) {
             return resultStmtVar;
         }
 
